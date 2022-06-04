04c353d3bacdc8d61b8fbafe97212d7061df4899
jamorera <jamorera1@misena.edu.co>
2022-06-04 11:24:16 -0500

    Update MedidaExterna.php

--- creato/app/Utility/MedidaExterna.php
+++ creato/app/Utility/MedidaExterna.php
@@ -2,20 +2,14 @@
 
 namespace App\Utility;
 
-use Illuminate\Support\Arr;
-
 class MedidaExterna
 {
     private $data;
     private $infoMadera;
-    public $espesorListon;
-    public $espesorTabla;
      public function __construct($data,$infoMadera)
      {
          $this->data = $data;
          $this->infoMadera = $infoMadera;
-         $this->espesorListon = $this->infoMadera['liston_costados']['espesor'];
-        $this->espesorTabla = $this->infoMadera['tabla_costados']['espesor'];
      }
     public function calcular(){  
         return [
@@ -27,7 +21,7 @@ class MedidaExterna
 
     function largo(){        
         return round($this->data['l_huacal'] + 
-        ($this->espesorListon + $this->espesorTabla)*2, 2);
+        ($this->infoMadera['liston_costados']['espesor']+ $this->infoMadera['tabla_costados']['espesor'])*2, 2);
     }
     function ancho(){        
         return round($this->data['a_huacal'] + 
